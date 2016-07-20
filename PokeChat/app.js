e5fc4776bd811f3ff30a98fe365dfa8a4fcf5914
Burak <burak.donbay@gmail.com>
2016-07-20 16:17:17 +0300

    Temp Passport

--- PokeChat/app.js
+++ PokeChat/app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+var chat = require('./routes/chat');
 
 var app = express();
 
@@ -22,8 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ PokeChat/routes/chat.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.route('/')
+    .post(function (req, res) {
+        res.send({ message: 'TODO: a new chat in mongo' });
+    })
+    .get(function (req, res) {
+        res.send({message: 'TODO: return all chat in mongo'});
+    });
+
+router.route('/:id')
+    .put(function (req, res) {
+        res.send({ message: 'TODO: update a chat and save it to mongo ' + req.params.id });
+    })
+    .get(function (req, res) {
+        res.send({ message: 'TODO: get a specific chat from mongo ' + req.params.id });
+    })
+    .delete(function (req, res) {
+        res.send({message: 'TODO: delete a specific chat from mongo ' + req.params.id});
+    })
+    ;
+
+module.exports = router;
