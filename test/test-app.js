44cf28435f1d84f9609617a7c9aed4decb6edc40
Burak <burak.donbay@gmail.com>
2016-07-30 21:31:47 +0300

    process.env. update

--- test/test-app.js
+++ test/test-app.js
@@ -28,6 +28,7 @@ describe('Pokes', function() {
             .end(function(err,res){
                 res.should.have.status(200);
                 res.should.be.json;
+                console.log("process.env.defaultChatMessage" + process.env.defaultChatMessage);
                 res.body.message.should.equal(process.env.defaultChatMessage);
                 done();
             });  
